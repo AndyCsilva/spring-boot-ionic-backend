7b5c20b2dba65102421faf727810cf931dd662c9
André Costa da Silva <andre91.net@gmail.com>
2018-04-09 01:25:33 -0300

    projeto criado

---
+++ src/main/java/com/nelioalves/cursomc/CursomcsApplication.java
@@ -0,0 +1,12 @@
+package com.nelioalves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcsApplication.class, args);
+	}
+}
