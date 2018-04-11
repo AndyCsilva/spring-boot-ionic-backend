437ae9da233dc27eb9ca24e0ebbcc42a0e77d493
André Costa da Silva <andre91.net@gmail.com>
2018-04-11 00:58:18 -0300

    testando o jax-rs

--- src/main/java/com/nelioalves/cursomcs/CursomcsApplication.java
+++ src/main/java/com/nelioalves/cursomcs/CursomcsApplication.java
@@ -1,4 +1,4 @@
-package com.nelioalves.cursomc;
+package com.nelioalves.cursomcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
