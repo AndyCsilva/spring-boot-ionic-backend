a8f2df8f90c918366adc2d5eb2df1699c004fbe9
André Costa da Silva <andre91.net@gmail.com>
2018-04-14 23:00:20 -0300

    criando operação de intanciação

--- src/main/java/com/nelioalves/cursomcs/CursomcsApplication.java
+++ src/main/java/com/nelioalves/cursomcs/CursomcsApplication.java
@@ -1,12 +1,32 @@
 package com.nelioalves.cursomcs;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcsApplication {
+import com.nelioalves.cursomcs.domain.Categoria;
+import com.nelioalves.cursomcs.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcsApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria informatica = new Categoria(null, "Informática");
+		Categoria escritorio = new Categoria(null, "Escritório");
+		
+		List<Categoria> categorias = Arrays.asList(informatica, escritorio);
+		categoriaRepository.saveAll(categorias);		
 	}
 }
