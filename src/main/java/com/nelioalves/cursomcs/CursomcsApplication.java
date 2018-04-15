1bf8b2fd457db953a9cb7bdf9d85ef1746d955bc
André Costa da Silva <andre91.net@gmail.com>
2018-04-15 00:17:21 -0300

    criando operação de associaçã de n x m

--- src/main/java/com/nelioalves/cursomcs/CursomcsApplication.java
+++ src/main/java/com/nelioalves/cursomcs/CursomcsApplication.java
@@ -9,7 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomcs.domain.Categoria;
+import com.nelioalves.cursomcs.domain.Produto;
 import com.nelioalves.cursomcs.repositories.CategoriaRepository;
+import com.nelioalves.cursomcs.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcsApplication implements CommandLineRunner {
@@ -17,6 +19,9 @@ public class CursomcsApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcsApplication.class, args);
 	}
@@ -26,7 +31,25 @@ public class CursomcsApplication implements CommandLineRunner {
 		Categoria informatica = new Categoria(null, "Informática");
 		Categoria escritorio = new Categoria(null, "Escritório");
 		
+		
+		//cria os produtos e persiste, depois associa
+		Produto p1 = new Produto(null, "Computador", 2000D);
+		Produto p2 = new Produto(null, "Impressora", 800D);
+		Produto p3 = new Produto(null, "Mouse", 80D);
+		
+		informatica.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		escritorio.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().add(informatica);
+		p2.getCategorias().add(informatica);
+		p3.getCategorias().add(informatica);
+		
+		p2.getCategorias().add(escritorio);
+			
 		List<Categoria> categorias = Arrays.asList(informatica, escritorio);
+		List<Produto> produtos = Arrays.asList(p1, p2, p3);
+		
 		categoriaRepository.saveAll(categorias);		
+		produtoRepository.saveAll(produtos);
 	}
 }
