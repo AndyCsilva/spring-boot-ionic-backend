7e7df2ceeb77f95b537013a3ccd7b320d8b5903c
André Costa da Silva <andre91.net@gmail.com>
2018-05-26 12:20:41 -0300

    Personalizando o retorno da Validação

--- src/main/java/com/nelioalves/cursomcs/resources/exceptions/ResorceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomcs/resources/exceptions/ResorceExceptionHandler.java
@@ -5,6 +5,8 @@ import java.time.Instant;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,7 +16,7 @@ import com.nelioalves.cursomcs.services.exceptions.ObjectNotFoundException;
 @ControllerAdvice
 public class ResorceExceptionHandler {
 
-	@ExceptionHandler(value =ObjectNotFoundException.class)
+	@ExceptionHandler(value = ObjectNotFoundException.class)
 	public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		
 		StandartError error = new StandartError(Instant.now(), HttpStatus.NOT_FOUND.value(),
@@ -23,11 +25,21 @@ public class ResorceExceptionHandler {
 	}
 
 	@ExceptionHandler(value = DataIntegrityException.class)
-	
 	public ResponseEntity<StandartError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		
 		StandartError error = new StandartError(Instant.now(), HttpStatus.BAD_REQUEST.value(),
 				HttpStatus.BAD_REQUEST.getReasonPhrase(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public ResponseEntity<StandartError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError error = new ValidationError(Instant.now(), HttpStatus.BAD_REQUEST.value(),
+				HttpStatus.BAD_REQUEST.getReasonPhrase(), "Erro de validação", request.getRequestURI());
+		for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+			error.addError(fieldError.getField() ,fieldError.getDefaultMessage());
+		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
--- src/main/java/com/nelioalves/cursomcs/resources/exceptions/StandartError.java
+++ src/main/java/com/nelioalves/cursomcs/resources/exceptions/StandartError.java
@@ -1,9 +1,14 @@
 package com.nelioalves.cursomcs.resources.exceptions;
 
+import java.io.Serializable;
 import java.time.Instant;
 
-public class StandartError {
+public class StandartError implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Instant timestamp;
 	private Integer status;
 	private String error;
